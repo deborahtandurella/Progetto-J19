808fc8ca65b46220a5d98dbbc104f7c1635e819a
MattiaBosio <mattiabosio20@gmail.com>
2019-06-07 12:34:26 +0200

    read menu files

--- src/main/java/net/Main.java
+++ src/main/java/net/Main.java
@@ -6,9 +6,10 @@ import application.RestaurantCatalogue;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Database.getInstance().addMenu(2,"menu_korte_dei_sapori.txt");
-        //Database.getInstance().addMenu(6,"M_torre_degli_aquila.txt");
-        //Database.getInstance().addMenu(5,"M_locanda_del_carmine.txt");
+        Database.getInstance().addMenu(2,"M_Korte_Dei_Sapori.txt");
+        Database.getInstance().addMenu(6,"M_torre_degli_aquila.txt");
+        Database.getInstance().addMenu(5,"M_locanda_del_carmine.txt");
+        Database.getInstance().addMenu(7,"Bardelli_&.txt");
         //RestaurantCatalogue.getInstance().printMenu(2);
         //RestaurantCatalogue.getInstance().printMenu(5);
         //RestaurantCatalogue.getInstance().printMenu(6);
