14234a254a7ec6bf158ef5c806b9d3739875bcae
Adriano <adriano.cotta.ramusino@gmail.com>
2019-06-08 16:38:10 +0200

    Added home ristoratore with stylesheet

--- src/main/java/net/RythmCliqueServlet.java
+++ src/main/java/net/RythmCliqueServlet.java
@@ -39,6 +39,9 @@ public class RythmCliqueServlet extends HttpServlet {
             case "/home_critico":
                 write(response, Rythm.render(("home_critico.html")));
                 break;
+            case "/home_ristoratore":
+                write(response, Rythm.render(("home_ristoratore.html")));
+                break;
             case "/home_utente":
                 write(response, Rythm.render(("home_utente.html")));
                 break;
